dd4fd95253c3af5e3e57f1bfbf49c95d9ed8ad5c
MrLumiere0 <hashidlouis@gmail.com>
2025-08-12 13:44:09 -0400

    deploy

--- src/pages/api/download.js
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-async function apihandler (request,response) 
-const apirequest = request
-{
-  response.json({ message: 'Response message' });
-}
